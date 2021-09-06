af6c90247938fd476c9e7a6d99402ea345696368
Gabriel_Jeronimo <gabrieljeronimo-@hotmail.com>
2021-09-06 10:54:53 -0300

    corrigindo o ft_lstsize

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: gjeronim <gjeronim@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 17:41:12 by gjeronim          #+#    #+#             */
-/*   Updated: 2021/09/04 19:11:05 by gjeronim         ###   ########.fr       */
+/*   Updated: 2021/09/06 10:53:47 by gjeronim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_lstsize(t_list *lst)
 	int	counter;
 
 	counter = 0;
-	while (lst->next)
+	while (lst)
 	{
 		lst = lst->next;
 		counter++;
