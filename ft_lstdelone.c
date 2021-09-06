94b7a41bb184786051c59b9f1744a57773c6c529
Gabriel_Jeronimo <gabrieljeronimo-@hotmail.com>
2021-09-06 10:59:40 -0300

    corrigindo o ft_lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: gjeronim <gjeronim@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 17:59:34 by gjeronim          #+#    #+#             */
-/*   Updated: 2021/09/04 19:08:50 by gjeronim         ###   ########.fr       */
+/*   Updated: 2021/09/06 10:58:47 by gjeronim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	del(lst->next);
-	free(lst);
+	if (lst && del)
+	{
+		del(lst->next);
+		free(lst);
+	}
+
 }
